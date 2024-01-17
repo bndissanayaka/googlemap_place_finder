1c70697f5399a93537e97ac9db8752d2a7f688ed
bndis <bhagya@lassana.com>
2024-01-17 16:03:22 +0530

    added a loading screen

--- src/components/js/frontPage.js
+++ src/components/js/frontPage.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../components/styles/frontPage.css";
 import { Link } from "react-router-dom";
 
 function FrontPage() {
+  const [onLoad, setOnLoad] = useState(false);
+  function onImageLoaded(url) {
+    const img = new Image();
+    img.src = url;
+    img.onload = function () {
+      setTimeout(() => {
+        setOnLoad(true);
+      }, "5000");
+    };
+  }
+  useEffect(() => {
+    onImageLoaded("./background.jpg");
+  }, []);
+
   return (
-    <div className="lv-frontpg-wrapper">
-      <div className="lv-frontpg-content">
-        Your sophisticated google maps place finder
-      </div>
-      <Link to="/landingPage">
-        <div className="lv-frontpg-link">Search your address</div>
-      </Link>
-    </div>
+    <>
+      {onLoad ? (
+        <div className="lv-frontpg-wrapper">
+          <div className="lv-frontpg-content">
+            Your sophisticated google maps place finder
+          </div>
+          <Link to="/landingPage">
+            <div className="lv-frontpg-link">Search your address</div>
+          </Link>
+        </div>
+      ) : (
+        <div className="lv-loading-img">
+          <img src="./marker.svg" alt="logo" className="lv-loading-marker" />
+          <p className="lv-frontpg-waitMsg"> Please wait...</p>
+        </div>
+      )}
+    </>
   );
 }
 
